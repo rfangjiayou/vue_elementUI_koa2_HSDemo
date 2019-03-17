21a353cea09594791dd7d058df5405093df44dbd
rfangjiayou <41361475+rfangjiayou@users.noreply.github.com>
2019-03-17 21:36:14 +0800

    Add files via upload

---
+++ sql/hsdemo.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.19-MariaDB : Database - hsdemo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hsdemo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `hsdemo`;
+
+/*Table structure for table `cr_policy` */
+
+DROP TABLE IF EXISTS `cr_policy`;
+
+CREATE TABLE `cr_policy` (
+  `name` varchar(128) DEFAULT NULL,
+  `actionDirection` int(4) DEFAULT NULL,
+  `match` blob,
+  `action` int(4) DEFAULT NULL,
+  `replement` varchar(1024) DEFAULT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cr_policy` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
