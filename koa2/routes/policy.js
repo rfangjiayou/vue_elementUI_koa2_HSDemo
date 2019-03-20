e6d166d7831bdfbe4927723cfde067afe4952941
rfangjiayou <41361475+rfangjiayou@users.noreply.github.com>
2019-03-20 22:13:37 +0800

    Add files via upload

--- koa2/routes/policy.js
+++ koa2/routes/policy.js
@@ -4,5 +4,6 @@ let controllerPolicy = require('../controller/policy');
 // router.prefix('/policy');
 
 router.get('/getObject', controllerPolicy.getObject);
+router.get('/createObject', controllerPolicy.createObject);
 
 module.exports = router;
