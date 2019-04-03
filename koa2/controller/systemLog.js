d517c40ecaceb702b14c5481482897dad86c525c
rfangjiayou <41361475+rfangjiayou@users.noreply.github.com>
2019-04-03 18:57:26 +0800

    Add files via upload

--- koa2/controller/systemLog.js
+++ koa2/controller/systemLog.js
@@ -18,6 +18,22 @@ class controllerSyslog {
         }
     }
 
+    static async getAttackType (ctx, next) {
+        try {
+            let query = ctx.query;
+            let returnData = await syslogModel.getObjectByLimit(query);
+            ctx.body = {
+                result : returnData.rows,
+                total : returnData.count
+            };
+        } catch (err) {
+            ctx.response.status = err.statusCode || err.status || 500;
+            ctx.response.body = {
+                message: err.message
+            };
+        }
+    }
+
     static async createObject (ctx, next) {
         try {
             let req = ctx.request.body;
